08c30659868374fe1aded2a5b6bb587bc0c00396
MarcoMadeddu <marcomade99@gmail.com>
2020-04-19 19:41:11 +0200

    js done

--- assets/main.js
+++ assets/main.js
@@ -3,12 +3,22 @@ $(document).ready(function (){
   var list = $(".list-chat");
   var newMessage = $(".new-message");
   var chatContainer = $(".main-right");
-  var sendButton =$(".footer-right .send-section .mic");
+  var sendButton =$(".footer-right .send-section .mic i");
 
   // S E N D  B Y  B U T T O N //
   sendButton.click(function(){
     addMessage();
-  })
+  });
+
+  // C H A N G E  S E N D  I C O N //
+  newMessage.focusin(function(){
+    sendButton.removeClass("fas fa-microphone");
+    sendButton.addClass("fas fa-paper-plane");
+  });
+  newMessage.focusout(function(){
+    sendButton.removeClass("fas fa-paper-plane");
+    sendButton.addClass("fas fa-microphone");
+  });
 
   // S E N D  B Y  K E Y  F U N C T I O N //
 
@@ -16,11 +26,13 @@ $(document).ready(function (){
     if(e.which==13){
       addMessage();
     }
-  })
+  });
 
 
   // F U N C T I O N S //
 
+
+// C R E A T E  N E W  M E S S A G E
   function addMessage(){
       var text = newMessage.val().trim();
     if(text!== ""){
@@ -30,6 +42,5 @@ $(document).ready(function (){
       chatContainer.scrollTop(chatContainer.prop('scrollHeight'));
     }
     newMessage.val("");
-  }
-
-})
+  };
+});
